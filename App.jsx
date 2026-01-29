15852e9b062d8c524a8f11386469ee873137da36
KARTIK PANDEY <kartik0pandey00@gmail.com>
2026-01-29 17:52:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import AdminDashboard from './pages/AdminDashboard';
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+          <Footer />
+        </div>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
